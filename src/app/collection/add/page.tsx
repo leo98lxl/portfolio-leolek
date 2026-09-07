0025811476b7e34a5e652f030aa215cba62e56f8
Leo Leksell <279720782+leo98lxl@users.noreply.github.com>
2026-09-07 16:31:43 +0200

    added form server action and validation

--- src/app/collection/add/page.tsx
+++ src/app/collection/add/page.tsx
@@ -1,42 +1,27 @@
-import Link from "next/link";
+import AddItemForm from "@/app/components/AddItemForm";
+import { createClient } from "@/app/utils/supabase/server";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function AddItem() {
+export default async function AddItem() {
+    const cookieStore = await cookies();
+    const supabase = createClient(cookieStore);
+    
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+        
+    if (!user) {
+      redirect('/account/log-in');
+    }
+
     return (
         <div>
             <h2>Add to Collection</h2>
             <p>Use the search field to look for a book. A successful match fills in all the required info fields for you.</p>
 
-            <form action="">
-                <label htmlFor="search">Search</label>
-                <input type="text" id="search" name="search" placeholder="Search for title, author, year..." autoFocus />
-
-                <label htmlFor="title">Title</label>
-                <input type="text" id="title" name="title" placeholder="Title of book" />
-
-                <label htmlFor="author">Author</label>
-                <input type="text" id="author" name="author" placeholder="Author of book" />
-
-                <label htmlFor="date">Release date</label>
-                <input type="date" id="date" name="date" placeholder="Release date" />
-
-                <fieldset>
-                    <legend>Status (Optional)</legend>
-                    <div>
-                        <input type="checkbox" id="status" name="status" />
-                        <label htmlFor="status">I have read this book</label>
-                    </div>
-                </fieldset>
-
-                <label htmlFor="rating">Your rating (Optional)</label>
-                <input type="number" id="rating" name="rating" min={1} max={5} />
-
-                <label htmlFor="review">Review (Optional)</label>
-                <textarea id="review" name="review" minLength={0} maxLength={200} placeholder="Write a review (up to 200 characters)">
-                </textarea>
-
-                <button type="submit">Add to Collection</button>
-                <Link href="/collection">Cancel</Link>
-            </form>
+            <AddItemForm>
+            </AddItemForm>
         </div>
     )
 }
